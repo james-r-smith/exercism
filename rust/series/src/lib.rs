8c79bff879f31bd95105334e63cea1d6f3479c6c
James Smith <jrs.james.smith@gmail.com>
2020-05-11 19:36:30 -0500

    feat(rust): add series

---
+++ rust/series/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn series(digits: &str, len: usize) -> Vec<String> {
+    if len == 0 {
+        return vec!["".to_string(); digits.len() + 1];
+    } else if len > digits.len() {
+        return vec![];
+    }
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|x| x.iter().collect())
+        .collect()
+}
