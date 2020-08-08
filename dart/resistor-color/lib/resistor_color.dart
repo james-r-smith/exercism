d41e0e0b361840bb2cf1c1f00e527f76da708d70
James Chen-Smith <jrs.james.smith@gmail.com>
2020-08-08 11:08:43 -0500

    feat(dart): add resistor-color

---
+++ dart/resistor-color/lib/resistor_color.dart
@@ -0,0 +1,18 @@
+class ResistorColor {
+  final List<String> colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int colorCode(String colorName) {
+    return colors.indexOf(colorName);
+  }
+}
