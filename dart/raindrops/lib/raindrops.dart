1fb7b595f765fe92d13b8e47447d55eca92a3b98
James Chen-Smith <jrs.james.smith@gmail.com>
2020-08-09 13:41:38 -0500

    feat(dart): add raindrops

---
+++ dart/raindrops/lib/raindrops.dart
@@ -0,0 +1,15 @@
+class Raindrops {
+  String convert(int number) {
+    String sound = '';
+    if (number % 3 == 0) {
+      sound += 'Pling';
+    }
+    if (number % 5 == 0) {
+      sound += 'Plang';
+    }
+    if (number % 7 == 0) {
+      sound += 'Plong';
+    }
+    return sound.isNotEmpty ? sound : '$number';
+  }
+}
